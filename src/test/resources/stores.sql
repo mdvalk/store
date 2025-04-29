05d6994f028f01aec1d861b9210401f3ba3e1933
Melvin de Valk <melvin.de.valk@outlook.com>
2025-04-29 13:06:12 +0200

    seed IT in-memory database with subset of data and add IT scenario

---
+++ src/test/resources/stores.sql
@@ -0,0 +1,45 @@
+TRUNCATE TABLE "stores" CASCADE;
+
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Oosterhout', '4907 XT', 'Arkendonk', '36', '', 'Jumbo Oosterhout Arkendonk', '5jgKYx4XKTUAAAFIimwYwKxK', '0101000020E6100000A375543541741340AF230ED940D44940', 32081, true, '08:00', 'Supermarkt', null, 4886, '20:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Oosterhout', '4901 KJ', 'Nieuwe Bouwlingstraat', '2', '', 'Jumbo Oosterhout Nieuwe Bouwlingstraat', 'F7gKYx4XulEAAAFIxm0YwKxK', '0101000020E6100000AB967494836913404A0D6D0036D24940', 32082, true, '08:00', 'SupermarktPuP', true, 4887, '21:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Oosterhout', '4904 AS', 'Zuiderhout', '4-7a', '', 'Jumbo Oosterhout Zuiderhout', 'YC8KYx4XB88AAAFIDcIYwKxJ', '0101000020E6100000DD990986736D134090BFB4A84FD04940', 33164, true, '08:00', 'Supermarkt', null, 3065, '20:00');
+
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Teteringen', '4847 EZ', 'Scheperij', '3', '', 'Jumbo Teteringen Scheperij', 'ewYKYx4XPZoAAAFLnMsJ5imL', '0101000020E61000001C7DCC07044A1340A148F7730ACE4940', 33196, true, '08:00', 'SupermarktPuP', true, 3635, '20:00');
+
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Breda', '4816 GD', 'Baliendijk', '20', '', 'Jumbo Breda Baliendijk', 'k14KYx4XBhkAAAFIalsYwKxK', '0101000020E610000079060DFD132C13404776A565A4CC4940', 32015, true, '08:00', 'Supermarkt', null, 4838, '21:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Breda', '4815 HA', 'Belcrumweg', '5-7', '', 'Jumbo Breda Belcrumweg', 'aT0KYx4XVA0AAAFIQDEYwKxK', '0101000020E610000003081F4AB414134047C8409E5DCC4940', 31387, true, '08:00', 'Supermarkt', null, 4620, '20:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Breda', '4814 PN', 'Cypresstraat', '21', '', 'Jumbo Breda Cypresstraat', 'yh0KYx4XzMAAAAFIqSEYwKrH', '0101000020E6100000B03C484F910313405709168733CB4940', 30233, true, '08:00', 'Supermarkt', null, 4648, '20:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Breda', '4834 HE', 'De Burcht', '20', '', 'Jumbo Breda De Burcht', '9d8KYx4XR4IAAAFIXyEYwKxK', '0101000020E6100000D788601C5C3A1340AEF204C24EC94940', 33046, true, '08:00', 'SupermarktPuP', true, 3695, '20:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Breda', '4824 CS', 'Donk', '4', '', 'Jumbo Breda De Donk', 'BqUKYx4Xr3EAAAFIRhYYwKxK', '0101000020E6100000CEAACFD556FC1240228C9FC6BDCD4940', 33047, true, '08:00', 'SupermarktPuP', true, 3645, '21:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Breda', '4812 TA', 'Dr. Struyckenplein', '60', '', 'Jumbo Breda Dr. Struyckenplein', '6MYKYx4XkaYAAAFIFYMYwKxK', '0101000020E6100000EA79371614061340401361C3D3C94940', 32210, true, '08:00', 'Supermarkt', null, 4958, '21:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Breda', '4815 NZ', 'Rat Verleghstraat', '4', '', 'Jumbo Breda Foodmarkt', 'YKwKYx4XuWcAAAFLDIUJ5jPY', '0101000020E61000007F2F8507CDFE124026C3F17C06CC4940', 32160, true, '08:00', 'SupermarktPuP', true, 4750, '22:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Breda', '4813 BS', 'Pastoor van Spaandonkstraat', '60', '', 'Jumbo Breda WC Princenhage', 'kbcKYx4Xz1kAAAFKhtR0LVDS', '0101000020E6100000FD67CD8FBFF412403DB7D09508CA4940', 33049, true, '08:00', 'SupermarktPuP', true, 3137, '20:00');
+
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Tilburg', '5021 AV', 'AaBe-straat', '8', '', 'Jumbo Tilburg AaBe-straat', '2a8KYx4XayEAAAFJRJUFS1eI', '0101000020E61000002E3D9AEAC96C14407C9A931799C64940', 32161, true, '08:00', 'Supermarkt', null, 4708, '21:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Tilburg', '5032 AZ', 'Amer', '13', '', 'Jumbo Tilburg Amer', 'LKMKYx4XpmYAAAFIH8wYwKxJ', '0101000020E61000002EA9DA6E822F144054E0641BB8C54940', 32204, true, '08:00', 'Supermarkt', null, 3101, '20:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Tilburg', '5046 GA', 'Bart van Peltplein', '1', '', 'Jumbo Tilburg Bart van Peltplein', 'aiMKYx4XpbEAAAFI_FYYwKxK', '0101000020E6100000B7F1272A1B561440302E55698BC94940', 32103, true, '08:00', 'SupermarktPuP', true, 4825, '22:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Tilburg', '5021 LG', 'Broekhovenseweg', '74', '', 'Jumbo Tilburg Broekhovenseweg', 'AJAKYx4XAToAAAFIXnEYwKxK', '0101000020E6100000E109BDFE245E14405A4B0169FFC54940', 32104, true, '08:00', 'SupermarktPuP', true, 4905, '22:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Tilburg', '5043 RA', 'Heyhoefpromenade', '11-17', '', 'Jumbo Tilburg Heyhoef', 'jf8KYx4XLIsAAAFIEnMYwKxK', '0101000020E6100000EAAEEC82C105144076DF313CF6C94940', 32105, true, '08:00', 'SupermarktPuP', true, 4906, '21:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Tilburg', '5038 TS', 'Lieve Vrouweplein', '11-12', '', 'Jumbo Tilburg Lieve Vrouweplein', 'McMKYx4XV7sAAAFIMKsYwKxK', '0101000020E6100000FD304278B4511440994A3FE1ECC64940', 31427, true, '08:00', 'Supermarkt', null, 6456, '21:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Tilburg', '5022 EH', 'Pater van de Elsenplein', '105', '', 'Jumbo Tilburg Pater van de Elsenplein', '34kKYx4XJrYAAAFI1XMYwKxK', '0101000020E61000007D09151C5E601440F513CE6E2DC54940', 32107, true, '08:00', 'Supermarkt', null, 4908, '20:00');
+INSERT INTO public.stores (city, postal_code, street, street2, street3, address_name, uuid, geometry, complex_number, show_warning_message, today_open, location_type, collection_point, sap_store_id, today_close)
+VALUES ('Tilburg', '5032 MG', 'Rosa Castellanosstraat', '5', '', 'Jumbo Tilburg Rosa Castellanosstraat', 'nScKYx4XyrEAAAFIetYYwKxK', '0101000020E6100000CA6C9049462E1440D0622992AFC44940', 35001, true, '08:00', 'PuP', true, 8841, '20:00');
